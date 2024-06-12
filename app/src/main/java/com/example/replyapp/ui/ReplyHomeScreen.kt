e3da58169ea44e94bb6c5e2662e3f19312ee5ce0
Vjosa Curri <114615502+vjosac@users.noreply.github.com>
2024-06-12 11:30:34 +0200

    Change screen with state changes

--- app/src/main/java/com/example/replyapp/ui/ReplyHomeScreen.kt
+++ app/src/main/java/com/example/replyapp/ui/ReplyHomeScreen.kt
@@ -66,13 +66,21 @@ fun ReplyHomeScreen(
             text = stringResource(id = R.string.tab_spam)
         )
     )
-    ReplyAppContent(
-        replyUiState = replyUiState,
-        onTabPressed = onTabPressed,
-        onEmailCardPressed = onEmailCardPressed,
-        navigationItemContentList = navigationItemContentList,
-        modifier = modifier
-    )
+    if (replyUiState.isShowingHomepage) {
+        ReplyAppContent(
+            replyUiState = replyUiState,
+            onTabPressed = onTabPressed,
+            onEmailCardPressed = onEmailCardPressed,
+            navigationItemContentList = navigationItemContentList,
+            modifier = modifier
+        )
+    } else {
+        ReplyDetailsScreen(
+            replyUiState = replyUiState,
+            onBackPressed = onDetailScreenBackPressed,
+            modifier = modifier
+        )
+    }
 }
 
 @Composable
