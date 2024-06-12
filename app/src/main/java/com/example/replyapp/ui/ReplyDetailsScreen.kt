d7351a5128748675c687f5eda6f2d6ff8ccc5a2e
Vjosa Curri <114615502+vjosac@users.noreply.github.com>
2024-06-12 11:32:18 +0200

    Create custom back handler in ReplyDetailsScreen

--- app/src/main/java/com/example/replyapp/ui/ReplyDetailsScreen.kt
+++ app/src/main/java/com/example/replyapp/ui/ReplyDetailsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.replyapp.ui
 
 import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -42,6 +43,9 @@ fun ReplyDetailsScreen(
     onBackPressed: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    BackHandler {
+        onBackPressed()
+    }
     Box(modifier = modifier) {
         LazyColumn(
             contentPadding = WindowInsets.safeDrawing.asPaddingValues(),
